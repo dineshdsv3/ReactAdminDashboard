cd307faaeb7cd26e14ed3a0505205542579d07fa
dineshdsv3 <dineshdsv3@gmail.com>
2020-07-11 18:21:03 +0530

    Added basic dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
+import Dashboard from './Dashboard';
+
+export default function App() {
+  return (
+    <Dashboard />
+  );
+}
